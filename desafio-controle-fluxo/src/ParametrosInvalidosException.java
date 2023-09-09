a3fe1233eb9f5f6595dbd4075b043af2af48f677
Uendel Nascimento <carinha23@gmail.com>
2023-09-08 21:11:53 -0300

    criando um pequeno projeto para validação de processo

---
+++ desafio-controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+
+
+}
